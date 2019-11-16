435b171b9598a62705957c51806bfc8db5c57b53
Sikorski Krzysztof <Sikorski.Krzysztof@zabka.pl>
2019-11-16 11:27:19 +0100

    wspolna struktura

--- codes/dist_exploration_ks.R
+++ codes/dist_exploration_ks.R
@@ -1,17 +1,25 @@
 
-distances<
   distances %>% class
 full_data %>% class
 distances %>% class
 preferences %>% class
 
 
+distances %>% select(city, raspberry, lime) %>% View
 
-jujube
-raspberry
+
 
 
 distances_ks<- distances %>% na.zero( ) %>% select( - city)
 distances_ks %>% lapply(., as.numeric) %>% tbl_df %>% lapply(., is.na) %>% lapply(.,sum)
-odleglosci_miedzy_miastami<- distances %>% na.zero( ) %>% select( - city) %>% as.matrix()%>% %>% as.dist()
+odleglosci_miedzy_miastami<- distances %>% na.zero( ) %>% select( - city) %>% as.matrix()%>% as.dist()
 
+
+
+
+distances_ks<- distances %>% na.zero( ) 
+distances_ks<-distances_ks %>% lapply(., as.numeric) %>% tbl_df %>% na.zero( )%>% is.na %>% any
+asdf<- distances_ks %>% as.matrix()
+
+asdf %>% head
+asdf %>% dim
